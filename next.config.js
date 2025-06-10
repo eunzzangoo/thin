431eed4a2648a01fb55f0738070b70305287bf56
eunzzangoo <emilyp7370@gmail.com>
2025-06-11 01:29:36 +0900

    fix: GitHub Pages CSS 로딩 문제 해결

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  // GitHub Pages에서 CSS가 제대로 로드되도록 설정
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
