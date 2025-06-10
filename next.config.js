71d3270f8d21411109dc3b87497365973f23d9eb
eunzzangoo <emilyp7370@gmail.com>
2025-06-11 02:18:01 +0900

    변경사항항

--- next.config.js
+++ next.config.js
@@ -5,23 +5,8 @@ const nextConfig = {
   assetPrefix: '/thin/',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
   },
   trailingSlash: true,
-  // GitHub Pages에서 SPA 라우팅을 위한 설정
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/index.html',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig 
