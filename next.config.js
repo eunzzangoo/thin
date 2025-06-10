e3882718172270647271156181c186637c3b39cc
eunzzangoo <emilyp7370@gmail.com>
2025-06-11 01:19:29 +0900

    fix: GitHub Pages 배포 설정 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/thin' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/thin/' : '',
+  basePath: '/thin',
+  assetPrefix: '/thin/',
   images: {
     unoptimized: true,
   },
